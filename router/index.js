e86651ea874b3221d848899be3265d2e30cc1eb3
Pranshul Gupta <pranshulgupta2000@gmail.com>
2021-02-03 12:44:40 +0530

    first commit

--- router/index.js
+++ router/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const homeController = require('../controller/home_controller');
-const { urlencoded } = require('express');
+// const { urlencoded } = require('express');
 
 // to convert the form data into object
 router.use(express.urlencoded());
